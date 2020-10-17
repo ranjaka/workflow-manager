b1a97ff72f0c14d5c98d8fe471c6f4118cb13953
Ranjaka De Mel <ranjaka.demel@team.telstra.com>
2020-10-17 17:17:27 +1100

    feat: added camunda engine dependencies and setup camunda workflow manager

--- camunda-manager/src/main/java/com/personal/camundamanager/CamundaManagerApplication.java
+++ camunda-manager/src/main/java/com/personal/camundamanager/CamundaManagerApplication.java
@@ -1,9 +1,11 @@
 package com.personal.camundamanager;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication("camunda-manager")
 public class CamundaManagerApplication {
 
 	public static void main(String[] args) {
