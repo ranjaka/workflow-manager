b491aa319d131bbf9dbca851531096e9e9963fd3
Ranjaka De Mel <ranjaka.demel@team.telstra.com>
2020-10-17 14:10:05 +1100

    chore: updated folder structure

---
+++ camunda-manager/src/main/java/com/personal/camundamanager/CamundaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.camundamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaManagerApplication.class, args);
+	}
+
+}
---
+++ camunda-manager/src/test/java/com/personal/camundamanager/CamundaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.camundamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
