963fc43214c951dbb326cdfb88aa330f2b5a0acd
Ranjaka De Mel <ranjaka.demel@team.telstra.com>
2020-10-18 00:12:39 +1100

    feat: added SimpleWorkflow bpmn for testing

--- camunda-manager/src/main/java/com/personal/camundamanager/CamundaManagerApplication.java
+++ camunda-manager/src/main/java/com/personal/camundamanager/CamundaManagerApplication.java
@@ -1,15 +1,28 @@
 package com.personal.camundamanager;
 
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@EnableProcessApplication("camunda-manager")
+@EnableProcessApplication
 public class CamundaManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CamundaManagerApplication.class, args);
-	}
+    @Autowired
+    RepositoryService repositoryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaManagerApplication.class, args);
+    }
+
+    @EventListener
+    public void notify(final PostDeployEvent usused) {
+        final ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().processDefinitionKey("SimpleWorkflowProcess").singleResult();
+    }
 
 }
